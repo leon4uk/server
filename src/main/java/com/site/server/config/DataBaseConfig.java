0ac99725d23097168cc47121cf66e675092d20b9
leon4uk <leon4uk@gmail.com>
2016-03-08 17:30:37 +0300

    databaseconfig start

---
+++ src/main/java/com/site/server/config/DataBaseConfig.java
@@ -0,0 +1,36 @@
+package com.site.server.config;
+
+import com.oracle.webservices.internal.api.message.PropertySet;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories("com.site.server.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan("com.site.server")
+
+public class DataBaseConfig {
+
+    @Resource
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(env.getRequiredProperty("db.url"));
+        ds.setDriverClassName(env.getRequiredProperty("db.driver"));
+        ds.setUsername(env.getRequiredProperty("db.username"));
+        ds.setPassword(env.getRequiredProperty("db.password"));
+        return ds;
+    }
+}
