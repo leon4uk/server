5dd235dadb1847e5921f4384bafdc13130074296
leon4uk <leon4uk@gmail.com>
2016-03-08 19:41:36 +0300

    JSON added

--- src/main/java/com/site/server/controller/ServerController.java
+++ src/main/java/com/site/server/controller/ServerController.java
@@ -1,12 +1,15 @@
 package com.site.server.controller;
 
-import org.springframework.stereotype.Controller;
+import com.site.server.entity.Server;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/site")
 public class ServerController {
 
@@ -14,7 +17,15 @@ public class ServerController {
     @RequestMapping(value = "/get", method = RequestMethod.GET)
 
     @ResponseBody
-    public String getName(ModelMap model){
-     return "Hello";
+    public Server getName(ModelMap model){
+     return createMockServer();
+    }
+
+    private Server createMockServer() {
+        Server server = new Server();
+        server.setId(1);
+        server.setDate(new Date());
+        server.setTitle("Первый Title");
+        return server;
     }
 }
